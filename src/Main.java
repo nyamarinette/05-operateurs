7cfcf8760cc052666b143f5c92f7ce215d7e85a8
nyamarinette <nyamarinette2020@gmail.com>
2024-01-23 18:33:56 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,71 @@
+
+public class Main {
+    public static void main(String[] args) {
+        /* nous avons  un opérateur d'affectation
+        int age = 34;
+            Ici nous verons a la sortie l affichage d'un texte suivi de la valeur de notre variable.
+
+        System.out.println("Tu as " +age+ "ans");
+
+        }
+        }
+         */
+  /*   un exemple de modulo: à la sortie nous avons juste son reste qui est 1
+
+
+        int result = 9%2;
+        System.out.println(result);
+    }
+}
+   */
+/* Ici nous utiliserons des golcoins et savoir combien de goldcoins nous avons gagné
+
+        int goldcoins = 0;
+        System.out.println(goldcoins);
+
+
+        goldcoins = +10;
+        System.out.println(goldcoins);
+
+
+        goldcoins = +100;
+        System.out.println(goldcoins);
+    }
+}
+*/
+/*nous avons une post-incrémentation (x++)
+
+int nb1 = 1;
+int nb2 = nb1 ++; //nb2=nb1 ensuite nb1= +1
+        System.out.println(nb1);
+        System.out.println(nb2);
+        }
+        }
+*/
+
+/* nous avons une pré-incrimenation ( ++x )
+int nb1 = 1;
+int nb2 = ++nb1 ;// nb1+=1 ensuite nb2= nb1
+        System.out.println(nb1);
+        System.out.println(nb2);
+        }
+        }
+*/
+
+/*nous avons post-décrémination (x--)
+        int nb1 = 1;
+        int nb2 = nb1-- ;// nb1=-1 ensuite nb2= nb1
+        System.out.println(nb1);
+        System.out.println(nb2);
+    }
+}
+*/
+/*nous avons un pré-décrémination
+
+        int nb1 = 1;
+        int nb2 = --nb1 ;// nb1=-1 ensuite nb2= nb1
+        System.out.println(nb1);
+        System.out.println(nb2);
+    }
+}
+*/
